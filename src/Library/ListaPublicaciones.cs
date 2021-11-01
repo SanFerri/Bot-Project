496a77448bed6d96f160e8db2dbfb50a1c6d82ab
[AgustinAmorena] <agustin.amorena@correo.ucu.edu.uy>
2021-11-01 10:12:06 -0300

    Comentarios

--- src/Library/ListaPublicaciones.cs
+++ src/Library/ListaPublicaciones.cs
@@ -1,8 +1,18 @@
 using System.Collections.Generic;
 namespace ClassLibrary
 {
+    /// <summary>
+    /// Lista publicaciones es un structurer de publicación que posee dos metodos AddPublicacione y removePublicaciones
+    /// para añadir o remover elementos de una property de la clase llamada ListaPublicaciones, es el encargado
+    /// de llevar a cabo dichas tareas porque es el experto en conocer los residuos.
+    /// </summary>
     public class ListaPublicaciones
     {
+        /// <summary>
+        /// Property publicación, es una lista de instancias de Publicacion
+        /// que lleva el registro de las publicaciones de una empresa.
+        /// </summary>
+        /// <returns></returns>
         public List<Publicacion> listaPublicaciones = new List<Publicacion>();
 
         /// <summary>
--- src/Library/ListaUsuarios.cs
+++ src/Library/ListaUsuarios.cs
@@ -1,14 +1,34 @@
 using System.Collections.Generic;
 namespace ClassLibrary
 {
+    /// <summary>
+    /// ListaUsuario es el experto en conocer a todos los usuarios, y 
+    /// por el patron Expert este tambien es quien
+    /// posee la responsabilidad de agregar y/o remover Usuarios.
+    /// A su vez depende de IUsuario para agregar cualquier tipo de usuario (emprendedor,
+    /// administrador o usuarios).
+    /// </summary>
     public static class ListaUsuarios
     {
+        /// <summary>
+        /// Variable estatica Usuarios, porque es una lista de instancias de Usuarios
+        /// que lleva un registro de todos las usuarios que hay.
+        /// </summary>
+        /// <returns></returns>
         public static List<IUsuario> usuarios{get;set;} = new List<IUsuario>();
+        /// <summary>
+        /// AddUsuarios es el encargado de agregar usuarios a la lista.
+        /// </summary>
+        /// <param name="usuario"></param>
         public static void AddUsuario(IUsuario usuario)
         {
             usuarios.Add(usuario);
         }
 
+        /// <summary>
+        /// RemoveUsuario es el encargado de remover usuarios de la lista.
+        /// </summary>
+        /// <param name="usuario"></param>
         public static void RemoveUsuario(IUsuario usuario)
         {
             usuarios.Remove(usuario);
