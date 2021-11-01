0f1e40e2d95ac4c42ae084574362d1dae3603a35
Santiago Ferraro <santiago.ferraro@correo.ucu.edu.uy>
2021-11-01 09:49:47 -0300

    Algunos comentarios.

--- src/Library/Mercado.cs
+++ src/Library/Mercado.cs
@@ -1,13 +1,33 @@
 using System.Collections.Generic;
 namespace ClassLibrary
 {
+    /// <summary>
+    /// La clase mercado es estatica porque el esta conoce todas las publicaciones
+    /// existentes, indepedientemente de a que empresa pertenezcan. Como esta es
+    /// la experta en la informacion de todas las publicaciones tambien es la
+    /// encargada atraves de sus 2 metodo Add y Remove Mercado de agregar o remover publicaciones.
+    /// </summary>
     public static class Mercado
     {
+        /// <summary>
+        /// Property 
+        /// </summary>
+        /// <returns></returns>
         public static List<Publicacion> mercado = new List<Publicacion>();
+
+        /// <summary>
+        /// AddMercado, metodo para agregar publicaciones al mercado, designado por Expert.
+        /// </summary>
+        /// <param name="publicacion"></param>
         public static void AddMercado(Publicacion publicacion)
         {
             mercado.Add(publicacion);
         }
+
+        /// <summary>
+        /// RemoveMercado, metodo para remover publicaciones al mercado, designado por Expert.
+        /// </summary>
+        /// <param name="publicacion"></param>
         public static void RemoveMercado(Publicacion publicacion)
         {
             mercado.Remove(publicacion);
