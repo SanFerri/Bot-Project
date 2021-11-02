53ef31b530c76962aadd9fc17249b90c148d68f0
[AgustinAmorena] <agustin.amorena@correo.ucu.edu.uy>
2021-11-02 14:42:39 -0300

    Comentarios

--- src/Library/Buscador.cs
+++ src/Library/Buscador.cs
@@ -2,8 +2,18 @@ using System;
 using System.Collections.Generic;
 namespace ClassLibrary
 {
+    /// <summary>
+    /// Buscador es el experto en conocer las busquedas.
+    /// </summary>
     public static class Buscador
     {
+        /// <summary>
+        /// Variable estatica Buscar porque es una lista de instancia de publicación que lleva 
+        /// el registro de todas las busquedas que hay.
+        /// </summary>
+        /// <param name="tipo"></param>
+        /// <param name="ubicacion"></param>
+        /// <returns></returns>
         public static List<Publicacion> Buscar(string tipo, Ubicacion ubicacion)
         {
             List<Publicacion> ofertas = new List<Publicacion>();
--- src/Library/Empresario.cs
+++ src/Library/Empresario.cs
@@ -1,16 +1,41 @@
 namespace ClassLibrary
 {
     /// <summary>
-    /// Clase Empresario del tipo IUsuario, posee, ademas de las property id, name e invitacion de toda clase 
+    /// Clase Empresario del tipo IUsuario, posee, ademas de las property id e invitacion de toda clase 
     /// usuario, una property empresa que indica la empresa de la que participa el Empresario.
     /// </summary>
     public class Empresario : IUsuario
     {
+        /// <summary>
+        /// Es el encargado de conocer el nombre del empresario. 
+        /// </summary>
+        /// <value></value>
         public string nombre{get; set;}
+       
+        /// <summary>
+        /// Es el encargado de conocer la empresa de la que es parte.
+        /// </summary>
+        /// <value></value>
         public Empresa empresa{get; set;}
+        
+        /// <summary>
+        /// Es el encargado de conocer el id del empresario.
+        /// </summary>
+        /// <value></value>
         public int id{get; set;}
+        
+        /// <summary>
+        /// Es el encargado de conocer el valor de una invitación de acceso.
+        /// </summary>
+        /// <value></value>
         public int invitacion{get; set;}
-
+       
+        /// <summary>
+        /// Constructor de una instancia de Empresario.
+        /// </summary>
+        /// <param name="invitacion"></param>
+        /// <param name="nombre"></param>
+        /// <param name="empresa"></param>
         public Empresario(int invitacion, string nombre, Empresa empresa)
         {
             this.empresa = empresa;
