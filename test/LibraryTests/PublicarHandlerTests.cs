f9d2581a46c9013f3ab93ea6f65930ba3aa1e1b3
SanFerri <santiago.ferraro@correo.ucu.edu.uy>
2021-11-02 17:53:03 -0300

    Terminado Publicar

--- test/LibraryTests/PublicarHandlerTests.cs
+++ test/LibraryTests/PublicarHandlerTests.cs
@@ -6,6 +6,7 @@ namespace Tests
 {
     public class HandlerTests
     {
+        Residuo residuo;
         PublicarHandler handler;
         string message;
         Empresa empresa;
@@ -16,10 +17,12 @@ namespace Tests
         [SetUp]
         public void Setup()
         {
+            residuo = new Residuo("metal", 100);
             handler = new PublicarHandler(null);
             int invitacion = InvitationGenerator.Generate();
             ubicacion = new Ubicacion("Av. 8 de Octubre 2738");
             empresa = new Empresa("MercadoPrivado", ubicacion, 099679938);
+            empresa.residuos.AddResiduo(residuo);
             Usuario = new Empresario(invitacion, empresa);
             id = 12345678;
             Usuario.id = id;
@@ -47,6 +50,24 @@ namespace Tests
             IHandler result = handler.Handle(message, emprendedor.id, out response);
 
             Assert.That(response, Is.EqualTo("Usted no es un empresario, no puede usar el codigo..."));
+        }
+
+        [Test]
+        public void WorkingPublicarHandler()
+        {
+            Emprendedor emprendedor = new Emprendedor(34314458);
+            message = handler.Keywords[0];
+            string response;
+
+            IHandler result = handler.Handle(message, id, out response);
+            
+            message = ubicacion.direccion;
+            handler.Handle(message, emprendedor.id, out response);
+            message = residuo.tipo;
+            handler.Handle(message, id, out response);
+
+
+            Assert.That(response, Is.EqualTo($"Se ha publicado la oferta de {residuo.tipo} de la empresa {empresa.nombre}. En la ubicacion {ubicacion.direccion}"));
         }
     }
 }
