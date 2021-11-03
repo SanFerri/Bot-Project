89f4d78675bdf3064af40750217859e41c2118f9
SanFerri <santiago.ferraro@correo.ucu.edu.uy>
2021-11-03 08:59:39 -0300

    Test InvitarHandler y RegistrarseHandler

--- src/Library/Registrado.cs
+++ src/Library/Registrado.cs
@@ -5,51 +5,28 @@ namespace ClassLibrary
 {
     public static class Registrado
     {
-        public static void VerifyUser(IUsuario usuario, string message)
+        public static bool VerifyUser(int id)
         {
             bool registrado = false;
 
-            foreach(Empresario usuario1 in ListaEmpresarios.empresarios)
+            foreach(Empresario empresario in ListaEmpresarios.empresarios)
             {
-                if(usuario.id == usuario1.id)
+                if(id == empresario.id)
                 {
                     registrado = true;
-                    usuario = usuario1;
                 }
             }
             if(registrado == false)
             {
                 foreach(IUsuario usuario1 in ListaUsuarios.usuarios)
                 {
-                    if(usuario.id == usuario1.id)
+                    if(id == usuario1.id)
                     {
                         registrado = true;
-                        usuario = usuario1;
                     }
                 }
             }
-            
-            if(registrado == false)
-            {
-                bool eleccion = false;
-                    Console.WriteLine("No está registrado, ingrese una invitación si es parte de una empresa, en caso de no serlo responda con un no");
-                    Registrado.VerifyConversation(usuario, "previoInvitacion");
-                    while(eleccion = false)
-                    {
-                        if(UsuarioConversacion.usuarioConversacion[usuario].conversacion[-1] == "previoInvitacion")
-                        {
-                            foreach(Empresario usuario1 in ListaEmpresarios.empresarios)
-                            {
-                                if(usuario1.invitacion == Convert.ToInt32(message))
-                                {
-                                    usuario1.id = usuario.id;
-                                    eleccion = true;
-                                    Console.WriteLine("Se te ha registrado con exito");
-                                }                                            
-                            }
-                        }
-                    } 
-            }
+            return registrado;
         }
         public static void VerifyConversation(IUsuario usuario, string message)
         {
--- test/LibraryTests/PublicarHandlerTests.cs
+++ test/LibraryTests/PublicarHandlerTests.cs
@@ -7,9 +7,11 @@ namespace Tests
     {
         Residuo residuo;
         PublicarHandler handler;
+        InvitarHandler handler2;
         string message;
         Empresa empresa;
         Empresario Usuario;
+        Administrador Usuario2;
         Ubicacion ubicacion;
         int id;
 
@@ -18,6 +20,7 @@ namespace Tests
         {
             residuo = new Residuo("metal", 100);
             handler = new PublicarHandler(null);
+            
             int invitacion = InvitationGenerator.Generate();
             ubicacion = new Ubicacion("Av. 8 de Octubre 2738");
             empresa = new Empresa("MercadoPrivado", ubicacion, 099679938);
@@ -26,6 +29,12 @@ namespace Tests
             id = 12345678;
             Usuario.id = id;
             ListaEmpresarios.AddEmpresario(Usuario);
+
+            handler2 = new InvitarHandler(null);
+            int invitacion2 = InvitationGenerator.Generate();
+            Usuario2 = new Administrador(invitacion2);
+            Usuario2.id = id;
+            ListaAdministradores.AddAdministrador(Usuario2);
         }
 
         [Test]
@@ -59,14 +68,58 @@ namespace Tests
             string response;
 
             IHandler result = handler.Handle(message, id, out response);
-            
+            Assert.That(response, Is.EqualTo("Porfavor ingrese la direccion de los residuos."));
             message = ubicacion.direccion;
             handler.Handle(message, emprendedor.id, out response);
+            Assert.That(response, Is.EqualTo("Ahora dime sobre cual de tus residuos quieres publicar"));
             message = residuo.tipo;
             handler.Handle(message, id, out response);
 
 
             Assert.That(response, Is.EqualTo($"Se ha publicado la oferta de {residuo.tipo} de la empresa {empresa.nombre}. En la ubicacion {ubicacion.direccion}"));
+        }
+
+        [Test]
+        public void InvitarAdministradorHandle()
+        {
+            message = handler2.Keywords[0];
+            string response;
+
+            IHandler result = handler2.Handle(message, id, out response);
+
+            Assert.That(response, Is.EqualTo("¿Cual es el nombre de la empresa que quiere invitar?"));
+        }
+
+        [Test]
+        public void InvitarUsuarioHandle()
+        {
+            message = handler2.Keywords[0];
+            string response;
+
+            IHandler result = handler2.Handle(message, 38291203, out response);
+
+            Assert.That(response, Is.EqualTo("Usted no es un administrador, no puede usar el codigo..."));
+        }
+
+        [Test]
+        public void WorkingInvitarHandler()
+        {
+            message = handler2.Keywords[0];
+            string response;
+
+            IHandler result = handler2.Handle(message, Usuario2.id, out response);
+            
+            message = "WCDONALDS";
+            handler2.Handle(message, Usuario2.id, out response);
+            message = ubicacion.direccion;
+            handler2.Handle(message, Usuario2.id, out response);
+            message = "099328938";
+            handler2.Handle(message, Usuario2.id, out response);
+            message = "ok";
+            handler2.Handle(message, Usuario2.id, out response);
+
+
+            Assert.That(response, Is.EqualTo($"Se ha creado el empresario y esta es la invitacion que debe usar para acceder a su status: {handler2.invitacion}"));
         }
     }
 }
