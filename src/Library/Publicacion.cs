3d1fabdba6bc1fedab2b9575b58bc07842a15df9
[AgustinAmorena] <agustin.amorena@correo.ucu.edu.uy>
2021-11-15 09:55:22 -0300

    Fix Comments

--- src/Library/Publicacion.cs
+++ src/Library/Publicacion.cs
@@ -3,13 +3,13 @@ using System;
 namespace ClassLibrary
 {
     /// <summary>
-    /// Publicacion es una clase que conoce la informacion de empresa, residuo, la fecha
-    /// y ubicacion de una publicacion hecha por una X empresa.
+    /// Publicación es una clase que conoce la información de empresa, residuo, la fecha
+    /// y ubicación de una publicación hecha por una X empresa.
     /// </summary>
     public class Publicacion
     {
         /// <summary>
-        /// Property empresa, es la empresa que crea la publicacion.
+        /// Property empresa, es la empresa que crea la publicación.
         /// </summary>
         /// <value></value>
         public Empresa empresa{ get; set; }
@@ -21,13 +21,13 @@ namespace ClassLibrary
         public string habilitacion { get; set; }
 
         /// <summary>
-        /// Property residuo, es el residuo que se esta ofertando en la publicacion.
+        /// Property residuo, es el residuo que se esta ofertando en la publicación.
         /// </summary>
         /// <value></value>
         public Residuo residuo{ get; set; }
 
         /// <summary>
-        /// Property ubicacion, es la ubicacion de donde se encuentran los residuos de la oferta.
+        /// Property ubicación, es la ubicación de donde se encuentran los residuos de la oferta.
         /// </summary>
         /// <value></value>
         public Ubicacion ubicacion{ get; set; }
